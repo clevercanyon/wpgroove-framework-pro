58d84c2150300380dc513dad5af3b403b01ae0d0
Mike Taykle <miketaykle@o5p.me>
2022-03-28 02:27:23 -0400

    update

--- src/classes/Traits/A6t/App/Utilities/Licensing_Members.php
+++ src/classes/Traits/A6t/App/Utilities/Licensing_Members.php
@@ -43,36 +43,41 @@ use WP_Groove\{Framework_Pro as WPG_Pro};
  * @see   WPG_Pro\A6t\Plugin
  * @see   WPG_Pro\A6t\Theme
  */
-trait Update_Members {
+trait Licensing_Members {
 	/**
-	 * Gets WordPress installation data for updates.
+	 * Gets WordPress installation data.
 	 *
 	 * @since 2022-03-12
 	 *
-	 * @return array WordPress installation data for updates.
+	 * @return array WordPress installation data.
 	 */
-	final public function installation_data_for_updates() : array {
+	final public function installation_data() : array {
+		$is_multisite      = is_multisite();
+		$is_network_active = $this->is_network_active();
+
 		return [
 			'php_version'    => PHP_VERSION,
 			'php_extensions' => implode( ',', U\Env::loaded_extensions() ),
 
 			'wp_version'      => U\Env::wp_version(),
-			'wp_is_multisite' => is_multisite(),
+			'wp_is_multisite' => $is_multisite,
 
-			'wp_network_home_url' => rtrim( network_home_url(), '/' ),
+			'wp_network_home_url' => $is_multisite ? rtrim( network_home_url(), '/' ) : '',
 			'wp_home_url'         => rtrim( home_url(), '/' ),
 
 			'wp_active_plugins'         => implode( ',', U\Env::wp_active_plugins() ),
-			'wp_network_active_plugins' => implode( ',', U\Env::wp_network_active_plugins() ),
+			'wp_network_active_plugins' => $is_multisite ? implode( ',', U\Env::wp_network_active_plugins() ) : '',
 
 			'wp_active_themes'         => implode( ',', U\Env::wp_active_themes() ),
-			'wp_network_active_themes' => implode( ',', U\Env::wp_network_active_themes() ),
+			'wp_network_active_themes' => $is_multisite ? implode( ',', U\Env::wp_network_active_themes() ) : '',
 
 			'wp_app_type'              => $this->type,
 			'wp_app_slug'              => $this->slug,
 			'wp_app_version'           => $this->version,
-			'wp_app_is_network_active' => $this->is_network_active(),
-			'wp_app_license_key'       => $this->get_option( 'license_key' ),
+			'wp_app_is_network_active' => $is_network_active,
+			'wp_app_license_key'       => $is_network_active
+				? $this->get_site_option( 'license_key' )
+				: $this->get_option( 'license_key' ),
 		];
 	}
 }
